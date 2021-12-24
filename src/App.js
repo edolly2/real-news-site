83bec152f03735637474a3ab18691cbc7fb50263
dollyllama <edolly2@protonmail.com>
2021-12-24 01:15:11 -0600

    ""

--- src/App.js
+++ src/App.js
@@ -2,21 +2,8 @@ import "./App.css";
 
 import React, { useState } from "react";
 
-import ArchivesPage from "./_pages/ArchivesPage";
-import BidenPage from "./_pages/BidenPage";
-import ChinaPage from "./_pages/ChinaPage";
-import FactCheckPage from "./_pages/FactCheckPage";
 import Header from "./_components/Header";
-import HomePage from "./_pages/HomePage";
-import HypocrisyPage from "./_pages/HypocrisyPage";
-import LatestPage from "./_pages/LatestPage";
-import NationalNewsPage from "./_pages/NationalNewsPage";
 import NavigationPc from "./_components/NavigationPc";
-import PoliticsPage from "./_pages/PoliticsPage";
-import { Route } from "react-router";
-import RussiaPage from "./_pages/RussiaPage";
-import SuppressedNewsPage from "./_pages/SuppressedNewsPage";
-import UnbelievablePage from "./_pages/UnbelievablePage";
 
 const App = () => {
   const [sideBarMenuActive, setSideBarMenuActive] = useState(false);
